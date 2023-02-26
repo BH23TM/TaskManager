162666bbfc3c54bbd6fa01f30ee21275aceb7873
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2023-02-26 10:17:55 -0800

    removed bullets

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,17 +4,16 @@ const Layout = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
+        
+          
             <Link to="/">Home</Link>
-          </li>
-          <li>
+          
             <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
+          
+          
             <Link to="/login">Login</Link>
-          </li>
-        </ul>
+          
+        
       </nav>
 
       <Outlet />
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,3 +1,4 @@
+
 const Splash = () => {
     return (
     <h1>Splash Page</h1>)
