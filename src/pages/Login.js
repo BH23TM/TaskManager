05ce1f430a1c499effe61a20dfcf47f5399f1274
Rhino004 <113748661+Rhino004@users.noreply.github.com>
2023-02-26 15:33:09 -0800

    [commit message]

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,23 @@
+
+import './Login.css';
+import { Route } from 'react-router-dom';
 const Login = () => {
-    return( 
-    <h1>Login Page</h1>)
+    return(
+
+        <div class = 'box'>
+            <h1>Login</h1>
+            <div class = 'email'>
+            <p>Email</p>
+            <input type={'text'}></input>
+            </div>
+            <div class = 'password'>
+                <p>password</p>
+                <input type = {'text'}></input>
+            </div>
+            <button  class = 'signup'>Sign Up</button>
+
+        </div>
+    )
 }
 
 export default Login;
---
+++ src/pages/sign_up.js
@@ -0,0 +1,5 @@
+const sign_up = () =>{
+    return(
+    <h1>Sign Up</h1>)
+}
+export default sign_up;
