cf0132a75815e681ae0881e4e0b9c25f783a4618
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2023-02-26 13:02:43 -0800

    created splashpage

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,7 +1,21 @@
-
+import './Splash.css';
+import step1 from './images/step1.png';
+import step2 from './images/step2.png';
+import step3 from './images/step3.png';
 const Splash = () => {
     return (
-    <h1>Splash Page</h1>)
+        <div>
+            <div className = 'Title'> <h>Task-Manager-Online</h> </div>
+            
+            <div className = 'Description'> <h>A more functional way to improve your productivity</h> </div>
+
+            <div className="Instructions">
+                <div className = "Step1"> <img className='step1' src = {step1}/><p>1. Create tasks in a task list</p> </div>
+                <div className='Step2'><img className='step2' src = {step2}/> <p>2. Group tasks & set a deadline</p> </div>
+                <div className='Step3'> <img className='step3' src = {step3}/> <p>3. Mark your tasks as done before your deadline with reminders</p> </div>
+            </div>
+        </div>
+    )
 }
 
 export default Splash;
