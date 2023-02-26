2ac5e283186cfefab03f021d3a0e0ee4fd024db3
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2023-02-25 18:53:10 -0800

    Update Bundle.js

--- js/Bundle.js
+++ js/Bundle.js
@@ -1,5 +1,27 @@
+class Task{
+
+}
+
 class Bundle{
     constructor(){
         this.task_list = [];
+        bundle_name = '';
     }
+    constructor(bundle_name){
+        this.task_list = [];
+        this.bundle_name = bundle_name;
+    }
+
+    addTask(t){
+        this.task_list.push(t);
+    }
+    
+    removeTask(t){
+        for (i = 0; i < this.task_list.length();i++){
+            if( t == task_list[i]){
+                this.task_list.splice(i,1);
+            }
+        }
+    }
+    
 }
