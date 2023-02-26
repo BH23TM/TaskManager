494f42bc6f2769b25e734bb3131b21f889706f5e
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2023-02-26 02:51:58 -0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from './pages/Dashboard';
 import './App.css';
+import Splash from './pages/Splash';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <div className="App">
-      <p> Hello </p>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element = {<Layout/>}>
+          <Route index element = {<Splash/>}/>
+          <Route path = "Dashboard" element = {<Dashboard/>}/>
+          <Route path = "Login" element = {<Login/>}/> 
+        </Route>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
